c734262ceacaefb6427c8e2497dd4219e90548e4
DanRoberts <DanRoberts@localhost>
2015-10-14 19:17:52 +0100

    First stab at making it work. Already found one case that isn't covered (false positive), there is a failing unit test for it.

---
+++ ProductiveRage.RuleAttributes/ReturnValueMustNotBeIgnoredAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProductiveRage.RuleAttributes
+{
+    // TODO: Note about how it can be used on property getters/setter (though why would you on a setter) but not on the property itself
+    // TODO: Note about it only applying directly to methods (not automatically to derived class' methods if attribute on interface method)
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class ReturnValueMustNotBeIgnoredAttribute : Attribute { }
+}
